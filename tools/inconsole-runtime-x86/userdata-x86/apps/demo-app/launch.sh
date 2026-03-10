f5caddd78e23da181a9489b655d3c3263f36be5f
InGraw-Co <grawik8@gmail.com>
2026-03-10 19:42:10 +0100

    Usuwam lokalny folder, aby zamienić go na submoduł

--- tools/inconsole-runtime-x86/userdata-x86/apps/demo-app/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "Demo app launched at $(date)" >> "${INCONSOLE_DATA_ROOT:-./userdata-x86}/system/logs/demo-app.log"
--- tools/inconsole-runtime-x86/userdata-x86/apps/long-truncate-test/launch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "long-truncate-test launched at $(date)" >> "${INCONSOLE_DATA_ROOT:-./userdata-x86}/system/logs/long-truncate-test.log"
